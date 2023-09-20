a768b26e4bab277cf109b496cd903f395b0fd41d
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-09-20 11:06:49 -0300

    Vídeo 6.3

---
+++ src/main/java/br/com/alura/screensound/model/Musica.java
@@ -0,0 +1,46 @@
+package br.com.alura.screensound.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "musicas")
+public class Musica {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    @ManyToOne
+    private Artista artista;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    public void setArtista(Artista artista) {
+        this.artista = artista;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Música='" + titulo + '\'' +
+                ", artista=" + artista;
+    }
+}
---
+++ src/main/java/br/com/alura/screensound/model/TipoArtista.java
@@ -0,0 +1,7 @@
+package br.com.alura.screensound.model;
+
+public enum TipoArtista {
+    SOLO,
+    DUPLA,
+    BANDA;
+}
